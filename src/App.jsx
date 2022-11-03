94ce741a21dd2292b162bf0431fcff78a9868bfa
Maximilian <89244567+Scientific-Research@users.noreply.github.com>
2022-11-03 23:48:30 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.scss'
+
+function App() {
+
+  return (
+    <div className="App">
+    <h1>Test App</h1>
+    <p>This is a test app with Vite.</p>
+    </div>
+  )
+}
+
+export default App
